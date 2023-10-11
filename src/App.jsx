fbff223e8947dde5486d65e8b8d48b81b20e62b1
Dennis malware <davspencer30@gmail.com>
2023-10-11 12:53:06 +0300

    updates

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+
+const App = () => {
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-16 relative">
+        <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default App
